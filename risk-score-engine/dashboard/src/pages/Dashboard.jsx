4ab592cbad78b1e291df81d4fa44e0f465e0eb61
Vishnuvardhan4563e <vishnuvardhan4564@gmail.com>
2026-08-25 17:11:20 +0530

    Update Dashboard.jsx

---
+++ risk-score-engine/dashboard/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RiskDashboard from '../components/RiskDashboard';
+
+function Dashboard() {
+  return (
+    <div className="dashboard-page">
+      <RiskDashboard />
+    </div>
+  );
+}
+
+export default Dashboard;
